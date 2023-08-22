fe3e8c0f95cc60b7576af5b2d61aabdeb880ee47
santoriellofabrizio <103045365+santoriellofabrizio@users.noreply.github.com>
2023-08-22 21:15:32 +0200

    Update secondQuiz.py

--- secondQuiz.py
+++ secondQuiz.py
@@ -16,6 +16,7 @@ rand_group = subgroups[randrange(1, len(subgroups))]
 
 
 with st.form("select element of group"):
+
     guess = st.multiselect(f"what are the ETFs of {rand_group}?",options=ETFs.index,default=st.session_state.guess)
     submitted = st.form_submit_button("Submit")
     if submitted:
