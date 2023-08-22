f8ad8582c49158d6ac60f151705b046c1c043934
santoriellofabrizio <103045365+santoriellofabrizio@users.noreply.github.com>
2023-08-22 22:02:53 +0200

    Update secondQuiz.py

--- secondQuiz.py
+++ secondQuiz.py
@@ -68,7 +68,7 @@ with st.form("select element of group"):
     if submitted:
         answer = ETFs.loc[ETFs["Group"] == rand_group].index.tolist()
         right_answers = set(st.session_state.guess).intersection(set(answer))
-        st.write(answer)
+        st.write(f"ciao {answer}")
         st.write(right_answers)
         if len(right_answers) == 0:
             
