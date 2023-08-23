57800fc17608d8c65576a7aecf1a18e396ff590c
santoriellofabrizio <103045365+santoriellofabrizio@users.noreply.github.com>
2023-08-23 15:13:07 +0200

    Update secondQuiz.py

--- secondQuiz.py
+++ secondQuiz.py
@@ -85,8 +85,6 @@ if "selected" in st.session_state:
         guess = st.multiselect(f"what are the ETFs of {rand_group}?", options=ETFs.index)
         st.session_state.guess = guess
         submitted = st.form_submit_button("Submit")
-        st.write(guess)
-        st.write(answer)
         if submitted:
             answer = ETFs.loc[ETFs["Group"] == rand_group].index.tolist()
             right_answers = intersection(answer,guess)
