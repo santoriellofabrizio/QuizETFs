728989c048d9fc30a88d5820d1b3c2f1273649af
santoriellofabrizio <103045365+santoriellofabrizio@users.noreply.github.com>
2023-08-23 17:09:16 +0200

    Update secondQuiz.py

--- secondQuiz.py
+++ secondQuiz.py
@@ -75,8 +75,9 @@ with st.form("quiz"):
         rand_group = subgroups[randrange(0,len(subgroups))]
         st.title(f"{rand_group}")
         guess = st.multiselect(f"what are the ETFs of {rand_group}?", options=ETFs.index,key="guessing")
+        submit = st.form_submit_button("submit"
  
-if st.form_submit_button("submit"):
+if submit:
             st.write("you sel:", guess)
             answer = ETFs.loc[ETFs["Group"] == rand_group].index.tolist()
             right_answers = intersection(answer, st.session_state.guess)    
