d63898b5f550e76554069a0e98b28f94b2a85783
santoriellofabrizio <103045365+santoriellofabrizio@users.noreply.github.com>
2023-08-23 15:35:16 +0200

    Update secondQuiz.py

--- secondQuiz.py
+++ secondQuiz.py
@@ -86,7 +86,7 @@ if start:
     guess = st.multiselect(f"what are the ETFs of {rand_group}?", options=ETFs.index)
     if "guess" not in st.session_state:
         st.session_state.guess = guess
-    submitted = st.button("Submit")
+    submitted = st.Button("submit")
     st.write("you sel:",st.session_state.guess)
 
     if submitted:
